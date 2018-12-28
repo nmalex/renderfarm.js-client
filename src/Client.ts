ec233bc3201624f8b00be1759c597f71b915334f
alex.khoroshylov <alex.khoroshylov@avira.com>
2018-12-28 15:38:43 +0100

    implemented very basic session request

--- src/Client.ts
+++ src/Client.ts
@@ -1,30 +1,48 @@
 import { Session } from "./Session";
 
+const axios = require("axios");
+
+const apiVersion = 1;
+
 class Client {
     private session: Session;
-    public constructed: boolean;
-    public constructor() {
-        this.constructed = true;
+    private apiKey: string;
+    private workspace: string;
+    private baseUrl: string;
+
+    public constructor(apiKey: string, workspace: string) {
+        this.apiKey = apiKey;
+        this.workspace = workspace;
     }
-    public Connect(): Promise<boolean> {
-        return new Promise<boolean>(function (resolve, reject) {
-            setTimeout(function () {
-                resolve(true);
-            }, 1000);
-        }.bind(this));
-    }
-    public Open(): Promise<boolean> {
-        return new Promise<boolean>(function (resolve, reject) {
-            this.session = new Session();
-            this.session.Open()
-                .then(function (value) {
-                    console.log("Session opened");
-                    resolve(true);
-                }.bind(this))
-                .catch(function (err) {
-                    console.error("Failed to open session");
-                    reject();
-                }.bind(this));
+
+    public Connect(host: string, port: number): Promise<any> {
+        if (port) {
+            this.baseUrl = `https://${host}:${port}/v${apiVersion}`;
+        } else {
+            this.baseUrl = `https://${host}/v${apiVersion}`;
+        }
+
+        return new Promise(function(resolve, reject) {
+            axios.post(`${this.baseUrl}/session`, {
+                api_key: this.apiKey, 
+                workspace: this.workspace
+            }).then(function (response) {
+                if (!response || response.status !== 200) {
+                    reject(); //todo: provide error
+                    return;
+                }
+
+                if (response.data && response.data.id) { //todo: be consistent, return guid
+                    resolve(true); //todo: provide some success object
+                } else {
+                    console.log(response.data);
+                    reject(response.data);
+                }
+            }).catch(function (error) { //todo: streamline how error is returned from api
+                console.log(" >> here: ", error);
+                reject(error);
+            }); // end of axios.post promise
+
         }.bind(this));
     }
 }
