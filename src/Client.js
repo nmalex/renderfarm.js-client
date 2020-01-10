b7b8bd0f62bc3dddeb19585267bcf7ae2c44acf6
alex.khoroshylov <alex.khoroshylov@4dpipeline.com>
2020-01-10 13:47:13 +0100

    add scene and job to props of Client

--- src/Client.js
+++ src/Client.js
@@ -4,9 +4,14 @@ import Job from './Job'
 
 export default class Client {
     constructor(config) {
+        const protocol = config.protocol ? config.protocol : "https";
+        const port     = config.port     ? config.port     :  443;
+
         this.apiKey = config.apiKey;
-        this.baseUrl = config.protocol + "://" + config.host + ":" + config.port + "/v1";
+        this.baseUrl = protocol + "://" + config.host + ":" + port + "/v1";
         this.session = null;
+        this.scene = null;
+        this.job = null;
     }
 
     async openSession(workspaceGuid, sceneFilename) {
@@ -15,31 +20,43 @@ export default class Client {
     }
 
     async createScene(threejsSceneObj, threejsCameraObj) {
-        var scene = new Scene(this.baseUrl, this.apiKey);
-        return scene.post(this.session.guid, threejsSceneObj, threejsCameraObj);
+        this.scene = new Scene(this.baseUrl, this.apiKey);
+        return this.scene.post(this.session.guid, threejsSceneObj, threejsCameraObj);
     }
 
-    async createJob(cameraName, width, height, onStarted, onProgress, onImageReady) {
-        var job = new Job(this.baseUrl, this.apiKey);
-        var promise = job.post(this.session.guid, cameraName, width, height);
-        promise.then(function(){
-            var job2 = job;
+    async createJob(cameraName, width, height, onStarted, onProgress, onImageReady, onError) {
+        this.job = new Job(this.baseUrl, this.apiKey);
+
+        var promise = this.job.post(this.session.guid, cameraName, width, height);
+        promise.then(async function(){
+
+            var jobInfo = await this.job.get();
+            onStarted && onStarted(jobInfo.data);
+
             var interval = setInterval(async function(){
-                var jobInfo = await job2.get();
+                var jobInfo = await this.job.get();
                 if (jobInfo.data.state !== 'rendering') {
                     clearInterval(interval);
                     if (jobInfo.data.urls && jobInfo.data.urls.length > 0) {
-                        onImageReady(jobInfo.data.urls[0]);
+                        onImageReady && onImageReady(jobInfo.data.urls[0]);
+                    } else {
+                        onError && onError(jobInfo.data);
                     }
+                } else {
+                    onProgress && onProgress(jobInfo.data);
                 }
-            }.bind(this), 1000);
+            }.bind(this), 1250); // check job state interval
+
         }.bind(this));
+
         return promise;
     }
 
     async close() {
-        if (this.session) {
-            return this.session.close();
+        if (!this.session) {
+            return Promise.resolve();
         }
+
+        return this.session.close();
     }
 }
