56404e7c7b380ed1a33037d2c37e870ebf2524e5
alex.khoroshylov <alex.khoroshylov@4dpipeline.com>
2020-01-22 21:46:35 +0100

    add fallback for camera name

--- src/Job.js
+++ src/Job.js
@@ -9,12 +9,18 @@ export default class Job {
     async post(sessionGuid, threejsCameraObj, renderSettings) {
         return new Promise(async function(resolve, reject) {
             try {
+                const cameraJson = threejsCameraObj.toJSON().object;
+                if (!cameraJson.name) {
+                    // if user did not care to set camera name, we still need some
+                    cameraJson.name = "Camera001";
+                }
+
                 var response = await axios.post(this.baseUrl + '/job', {
                     session_guid: sessionGuid,
                     render_width: renderSettings.width,
                     render_height: renderSettings.height,
                     alpha: renderSettings.alpha,
-                    camera_json: threejsCameraObj.toJSON().object,
+                    camera_json: cameraJson,
                     render_settings: {
                         // use it to pass direct vray settings
                     }
